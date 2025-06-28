73e7735171ae6b7e9ebb82be51cca8694bd4dc32
AIChubar <achubar.work@gmail.com>
2025-06-28 22:51:01 +0200

    Added new items

---
+++ Assets/Editor/ShopConfigEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[CustomEditor(typeof(ShopConfig))]
+public class ShopConfigEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ShopConfig config = (ShopConfig)target;
+
+        if (GUILayout.Button("Populate with All Items"))
+        {
+            string[] guids = AssetDatabase.FindAssets("t:Item");
+            config.allItems = guids
+                .Select(guid => AssetDatabase.LoadAssetAtPath<Item>(AssetDatabase.GUIDToAssetPath(guid)))
+                .Where(item => item != null)
+                .ToList();
+
+            config.availableItems = new List<Item>(config.allItems);
+
+            EditorUtility.SetDirty(config);
+            Debug.Log($"Populated with {config.allItems.Count} items.");
+        }
+    }
+}
